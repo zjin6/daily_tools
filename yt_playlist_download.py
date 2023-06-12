20b5f1d54f5583257399fe38d4d9ba9f7043fb7b
Jimmy <jimmyjin.zjin6@gmail.com>
2023-06-12 21:25:43 +0800

    Update yt_playlist_download.py
    1. deleted codes for downloading subtitles, refactor only for video
    2. added to print downloading speed

--- yt_playlist_download.py
+++ yt_playlist_download.py
@@ -1,5 +1,4 @@
 from pytube import YouTube
-# from pytube.cli import on_progress
 from datetime import datetime
 import time
 import urllib.request
@@ -7,14 +6,8 @@ import re
 import pandas as pd
 
 
-# try to fix "Exception: IncompleteRead", seems does NOT work
-# import http.client
-# http.client.HTTPConnection._http_vsn = 10
-# http.client.HTTPConnection._http_vsn_str = 'HTTP/1.0'
-
-
-yt_link = input("Enter video collection link : ")
-save_path = input("Enter path to save : ")
+yt_link = input("enter url: ")
+save_path = input("path to save: ")
 list_link = yt_link.split("?")[1]
 html = urllib.request.urlopen(yt_link)
 
@@ -24,15 +17,12 @@ video_ids = list(video_ids)
 qty_video = len(video_ids)
 
 
-
 i = 0
-dead_link_trail = 0
+dead_link_trail = 0 # try 'IncompleteRead' for 5 times
 incomp_list = []
 while i < qty_video:
     current_time = datetime.now().strftime("%H:%M")
     print("\n" + current_time)
-    
-    # url_video = "https://www.youtube.com/watch?v=" + video_ids[i] + "&" + list_link + "&index=" + str(i+1)
     url_video = "https://www.youtube.com/watch?v=" + video_ids[i]
     print(url_video)
 
@@ -44,31 +34,19 @@ while i < qty_video:
             minutes_left = round(time_spent.total_seconds() / progress_percent * (100 - progress_percent) /60, 1)
             print(str(progress_percent) + '% | ' + str(minutes_left) + 'mins ... ', end='')        
                 
+        start = datetime.now()        
         yt = YouTube(url_video, on_progress_callback = progress_callback)
-        start = datetime.now()
         print('Title: ',yt.title)
         ys = yt.streams.get_highest_resolution()
         print("download Video ... " + str(int(ys.filesize/10**6)) + ' MB')
         ys.download(save_path) 
         
-        print('\n' + 'download Subtitle ...')
-        if 'en' in yt.captions:
-            caption = yt.captions['en']
-        elif 'a.en' in yt.captions:
-            caption = yt.captions['a.en']
-        else:
-            print('no Subtitle, jump to next ...')
-            i += 1
-            continue
-            
-        caption_text = caption.generate_srt_captions()
-        filename = re.sub('[<>:\/\\\|?*"#,.\']+', '', yt.title) + '.srt'
-        caption_path = save_path + '\\' + filename
-        with open(caption_path, 'w', encoding='utf-8') as f:
-            f.write(caption_text)
-              
-        time_spent = datetime.now() - start
-        print('time: ' + str(time_spent).split('.')[0])  
+        total_time_spent = datetime.now() - start
+        total_seconds = total_time_spent.total_seconds()
+        download_speed = ys.filesize/10**6 / total_seconds
+        print('time spent: ' + str(total_time_spent).split('.')[0])          
+        print(f'overall speed: {download_speed:.1f} MB/second')
+
         i += 1 
         dead_link_trail = 0        
         
@@ -89,47 +67,7 @@ while i < qty_video:
 
 
 current_time = datetime.now().strftime("%H:%M")
-print("\n" + current_time)
+print("closed at " + current_time)
 print('Incompleted videos: ---------------')
-incomp_string = ''
 for title, url in incomp_list:
-    to_string = title + ': ' + url + '\n'
-    incomp_string += to_string
-    print(to_string, end='')
-
-
-incomp_list_path = save_path + '\\' + 'Incompleted videos: ---------------'
-with open(incomp_list_path, 'w', encoding='utf-8') as f:
-    f.write(incomp_string)
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    print(title + ': ' + url + '\n')
