4b7264e85dbb825a39b8a7aa901fbfb09d7efb6d
Jimmy <jimmyjin.zjin6@gmail.com>
2023-06-17 16:17:23 +0800

    Update youtube_subtitle.py
    use
    @retry(max_attempts=12, sleep_time=5)
    def english_subtitles(video_id, file_path)
    replaced
    def try_func(func_name, video_id, file_path, try_times=12),
    so exceptions are handed by @retry(max_attempts=12, sleep_time=5)

--- youtube_subtitle.py
+++ youtube_subtitle.py
@@ -19,13 +19,6 @@ def pull_html(yt_link):
     print('html ready ...')    
     return html
 
-@retry(max_attempts=None, sleep_time=0)
-def pull_video_title(url_video):
-    video_title = YouTube(url_video).title
-    print(video_title) 
-    return video_title
-
-
 html = pull_html(yt_link)
 video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
 video_ids = pd.Series(video_ids).drop_duplicates()
@@ -33,44 +26,32 @@ video_ids = list(video_ids)
 qty_video = len(video_ids)
 print(qty_video, 'videos found.')
 
-  
+
+@retry(max_attempts=None, sleep_time=0)
+def pull_video_title(url_video):
+    video_title = YouTube(url_video).title
+    print(f'Title: {video_title}') 
+    return video_title
+
+
+@retry(max_attempts=12, sleep_time=5)  
 def english_subtitles(video_id, file_path):
-    try:
-        # Get the transcript for the YouTube video in English
-        transcript_en = YouTubeTranscriptApi.get_transcript(video_id, languages=['en'])
+    # Get the transcript for the YouTube video in English
+    transcript_en = YouTubeTranscriptApi.get_transcript(video_id, languages=['en'])
 
-        # Create an English .srt file
-        with open(file_path, 'w', encoding='utf-8') as f:
-            # Write each English subtitle entry to the .srt file
-            for index, line in enumerate(transcript_en):
-                start_time = line['start']
-                end_time = line['start'] + line['duration']
-                subtitle_text = line['text']
-                subtitle_entry = f"{index + 1}\n{convert_time(start_time)} --> {convert_time(end_time)}\n{subtitle_text}\n\n"
-                f.write(subtitle_entry)
+    # Create an English .srt file
+    with open(file_path, 'w', encoding='utf-8') as f:
+        # Write each English subtitle entry to the .srt file
+        for index, line in enumerate(transcript_en):
+            start_time = line['start']
+            end_time = line['start'] + line['duration']
+            subtitle_text = line['text']
+            subtitle_entry = f"{index + 1}\n{convert_time(start_time)} --> {convert_time(end_time)}\n{subtitle_text}\n\n"
+            f.write(subtitle_entry)
 
-        print(f"saved: {file_path}")
-        return True
-
-    except Exception as e:
-        print("Exception YouTubeTranscriptApi.get_transcript --------------- \n" + str(e))        
-        
-        if 'is unavailable' in str(e) or 'member' in str(e) or 'streamingData' in str(e):
-            print("no authority to access, skip ...")  
-            return True
-           
-        elif 'IncompleteRead' in str(e):
-            print('try again ... ')
-            return False        
-       
-        elif 'HTTP Error 429' in str(e):           
-            for wait in range(5):
-                print(wait, end=' ')
-                time.sleep(1)
-            return False
-        else:
-            print('unknown exception to check ...')
-            return False
+    print(f"saved: {file_path}")
+    print('next subtitle ...')    
+    return f
 
 
 def convert_time(seconds):
@@ -80,18 +61,6 @@ def convert_time(seconds):
     seconds = int(seconds % 60)
     milliseconds = int((seconds % 1) * 1000)
     return f"{hours:02d}:{minutes:02d}:{seconds:02d},{milliseconds:03d}"
-
-
-def try_func(func_name, video_id, file_path, try_times=12):
-    run = 0
-    while run < try_times:
-        print(f'run {run + 1}, ', end=' ')
-        run_func = func_name(video_id, file_path)
-        if run_func:
-            break
-        else:
-            run += 1
-    print('next subtitle ...')
 
 
 for video_id in video_ids:
@@ -105,4 +74,4 @@ for video_id in video_ids:
     filename = re.sub('[<>:\/\\\|?*"#,.\']+', '', video_title) + '.srt'
     
     file_path = os.path.join(save_path, filename)  
-    try_func(english_subtitles, video_id, file_path)
+    english_subtitles(video_id, file_path)
