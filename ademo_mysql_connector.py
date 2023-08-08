1d6fd998227e02b549badf79c62309ade4242f7b
Jimmy <jimmyjin.zjin6@gmail.com>
2023-08-08 18:13:31 +0800

    Create ademo_mysql_connector.py
    a demo code

---
+++ ademo_mysql_connector.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="jin365",
+    database="newbase"
+)
+
+
+mycursor = mydb.cursor()
+mycursor.execute("SELECT * FROM newtable")
+myresult = mycursor.fetchall()
+for x in myresult:
+    print(x)
+
