0fb6faeddecc8eb3e552176c20b9815be0057f1f
Jimmy <90012626+zjin6@users.noreply.github.com>
2024-04-08 12:04:46 +0800

    check line by line yourself

--- findip.py
+++ findip.py
@@ -4,12 +4,10 @@ import subprocess
 import time
 from datetime import datetime
 
-start = datetime.now()
 
-
-def findip(n):
+def findip(n, subnet='10.0.0.' ):
     for i in range(n*3, (n+1)*3):
-        command_line = 'ping -a -n 1 ' + '10.0.0.' + str(i)
+        command_line = 'ping -a -n 1 ' + subnet + str(i)
         # command_line = 'ping -4 -a -n 1 ' + 'WGC100DFH0NQ2'
         # print(command_line)
         try:
@@ -20,19 +18,27 @@ def findip(n):
             print(' empty_ip .' + str(i))
 
 
-if __name__ =="__main__":
-    
-    for n in range(5):
-        globals()[f'threading{n}'] = threading.Thread(target=findip, args=(n,))
+if __name__ =="__main__":  
+    subnet = input('subnet starts 10.0.0. ? : ')  
+    start = datetime.now()   
+
+    if subnet == '':
+        subnet = '10.0.0.'
+        scan_scope = 5
+    else:
+        scan_scope = int(input('scan scope 3x:'))
+        
+    for n in range(scan_scope):
+        globals()[f'threading{n}'] = threading.Thread(target=findip, args=(n, subnet))
         globals()[f'threading{n}'].start()
         # print('open threading ...', n)
     time.sleep(3)
-    for n in range(5):   
+    for n in range(scan_scope):   
         globals()[f'threading{n}'].join()
         # print('close threading ...', n)
 
 end = datetime.now()
-print(str(end - start).split('.')[0])
+print('scan time : ' + str(end - start).split('.')[0])
 
 
 
