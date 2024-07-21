d29a062195108b9d9bc2e5b7c9b238000c9c245c
Jimmy <90012626+zjin6@users.noreply.github.com>
2024-07-21 15:43:53 +0800

    Update yt_video_audio.py
    KhurramRana commented 4 days ago,
    
    Place these lines of code at the top of your codebase and the issue will be fixed.
    
    from pytube. innertube import _default_clients
    
    _default_clients[ "ANDROID"][ "context"]["client"]["clientVersion"] = "19.08.35"
    _default_clients["IOS"]["context"]["client"]["clientVersion"] = "19.08.35"
    _default_clients[ "ANDROID_EMBED"][ "context"][ "client"]["clientVersion"] = "19.08.35"
    _default_clients[ "IOS_EMBED"][ "context"]["client"]["clientVersion"] = "19.08.35"
    _default_clients["IOS_MUSIC"][ "context"]["client"]["clientVersion"] = "6.41"
    _default_clients[ "ANDROID_MUSIC"] = _default_clients[ "ANDROID_CREATOR" ]

--- yt_video_audio.py
+++ yt_video_audio.py
@@ -6,7 +6,14 @@ import pandas as pd
 import os, subprocess
 import csv
 from request_retry import retry, list_failed
+from pytube. innertube import _default_clients
 
+_default_clients[ "ANDROID"][ "context"]["client"]["clientVersion"] = "19.08.35"
+_default_clients["IOS"]["context"]["client"]["clientVersion"] = "19.08.35"
+_default_clients[ "ANDROID_EMBED"][ "context"][ "client"]["clientVersion"] = "19.08.35"
+_default_clients[ "IOS_EMBED"][ "context"]["client"]["clientVersion"] = "19.08.35"
+_default_clients["IOS_MUSIC"][ "context"]["client"]["clientVersion"] = "6.41"
+_default_clients[ "ANDROID_MUSIC"] = _default_clients[ "ANDROID_CREATOR" ]
 
 
 @retry(max_attempts=None, sleep_time=0)
